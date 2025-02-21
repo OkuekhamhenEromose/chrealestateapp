3b4adf69807e9c6133087ccef92f721a22629ba6
Charles Eromose <okuekhamhene.gmail.com>
2025-02-21 02:26:45 +0100

    latest commit

--- api/settings.py
+++ api/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://chrealestateapp.onrender.com','localhost:8000','127.0.0.1:8000']
+ALLOWED_HOSTS = ['https://chrealestateapp.onrender.com']
 
 
 # Application definition
